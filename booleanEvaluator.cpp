0e4a6b0a9d4ebfddd1a36d3481fe02e0c8290619
Mohammad Momenuzzaman <bsse1227@iit.du.ac.bd>
2022-03-05 19:12:00 +0600

    Updates for 5-3-2022

--- booleanEvaluator.cpp
+++ booleanEvaluator.cpp
@@ -1,10 +1,93 @@
 #include<bits/stdc++.h>
 
+#include<graphics.h>
+
+
+
 using namespace std;
 int main(){
-    string boolExp;
+    string inputExp;
     cout<<"Please enter expression to be evaluated: ";
-    cin>>boolExp;
+    cin>>inputExp;
 
+    /*//karnaugh map to solve boolean problem
+
+    //initialize the map
+    int map[4][4];
+    for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
+            map[i][j]=0;
+        }
+    }
     
+    //initialize the truth table
+    int truthTable[16];
+    for(int i=0;i<16;i++){
+        truthTable[i]=0;
+    }*/
+
+    //initialize string to store input expression
+    
+    //converting string to Chomsky normal form
+    string cnfExp;
+    cnfExp=inputExp;
+    int cnfExpLength=cnfExp.length();
+    for(int i=0;i<cnfExpLength;i++){
+        if(cnfExp[i]=='.'){
+            cnfExp[i]='^';
+        }
+    }
+
+
+    //initialize stack to store operators
+    stack<char> opStack;
+
+    //initialize stack to store operands
+    stack<char> opdStack;
+
+    //initialize stack to store result
+    stack<char> resStack;
+
+    //convert input expression to postfix 
+    for(int i=0;i<inputExp.length();i++){
+        if(inputExp[i]=='('){
+            opStack.push(inputExp[i]);
+        }
+        else if(inputExp[i]==')'){
+            while(opStack.top()!='('){
+                opdStack.push(opStack.top());
+                opStack.pop();
+            }
+            opStack.pop();
+        }
+        else if(inputExp[i]=='&' || inputExp[i]=='|' || inputExp[i]=='^'){
+            while(!opStack.empty() && opStack.top()!='('){
+                opdStack.push(opStack.top());
+                opStack.pop();
+            }
+            opStack.push(inputExp[i]);
+        }
+        else{
+            opdStack.push(inputExp[i]);
+        }
+    }
+    
+
+    //searching for AND and OR operators
+    for(int i=0; i<inputExp.length(); i++){
+        if(inputExp[i]=='+'){
+            cout<<"AND operator found at position "<<i<<endl;
+            //and gate logic diagram printing
+
+            }
+        else if(inputExp[i]=='|'){
+            cout<<"OR operator found at position "<<i<<endl;
+            //or gate logic diagram printing
+
+        }
+        else if(inputExp[i]=='^'){
+            cout<<"XOR operator found at position "<<i<<endl;
+            //xor gate logic diagram printing
+        }
+    }
 }
---
+++ easyBMPDemo.cpp
@@ -0,0 +1,44 @@
+#include "EasyBMP.hpp"
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+struct point
+{
+  double x, y;
+  point(double _x, double _y) : x(_x), y(_y) { }
+  double distanceTo(const point& o) {
+    double dx = (x - o.x);
+    double dy = (y - o.y);
+    return sqrt(dx * dx + dy * dy);
+  }
+};
+
+int main()
+{
+  vector< point > centers = {
+    {256, 256},
+    {192, 192}
+  };
+  vector< double > charges = {1024.0, 2048.0};
+  // R, G, B [0, 255]
+  EasyBMP::RGBColor black(0, 0, 0);  
+  // sizeX, sizeY, FileName, BackgroundColor
+  EasyBMP::Image img(512, 512, "sample.bmp", black);
+
+  for (int y = 0; y < 512; ++y) {
+    for (int x = 0; x < 512; ++x) {
+      double intensity = 0.0;
+      for (int i = 0; i < 2; ++i) {
+        double dist = max(centers[i].distanceTo(point(x, y)), 1.0);
+        intensity += charges[i] / (dist * dist);
+      }
+      int final_color = min(255, int(255. * intensity));
+      // PositionX, PisitionY, Color
+      img.SetPixel(x, y, EasyBMP::RGBColor(final_color, final_color, 0));
+    }
+  }
+  img.Write();
+  return 0;
+}
