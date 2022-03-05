099d6bd37a983e17111586f5bfc79220760fea80
Mohammad Momenuzzaman <bsse1227@iit.du.ac.bd>
2022-03-05 23:59:39 +0600

    .

--- booleanEvaluator.cpp
+++ booleanEvaluator.cpp
@@ -1,32 +1,57 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
-#define boolAdd(x) x + 0 = x;
+bool boolOR0(bool x)
+{
+    return x;
+}
+bool boolOR1(bool x)
+{
+    return 1;
+} 
+bool boolORSelf(bool x)
+{
+    return x;
+} 
+bool boolORComplement(bool x)
+{
+    return 1;
+} 
 
-#define boolAdd0(x) x + 1 = 1;
-
-#define boolAddSelf(x) x + x = x;
-
-#define boolAddComplement(x) x + !x = 1;
+bool boolAND1(bool x)
+{
+    return x;
+}
+bool boolAND0(bool x)
+{
+    return 0;
+}
+bool boolANDself(bool x) 
+{
+    return x;
+}
+bool boolANDcomplement(bool x) 
+{
+    return 0;
+}
 
 //#include<graphics.h>
 using namespace std;
-//function to calculate length of string
+// function to calculate length of string
 int strLength(string str)
 {
     int len = 0;
-    while(str[len] != '\0')
+    while (str[len] != '\0')
     {
         len++;
     }
     return len;
 }
 
-
 bool isOperator(char c)
 {
     return (!isalpha(c) && !isdigit(c));
 }
- 
+
 int getPriority(char C)
 {
     if (C == '-' || C == '+')
@@ -37,133 +62,145 @@ int getPriority(char C)
         return 3;
     return 0;
 }
- 
+
 string infixToPostfix(string infix)
 {
     infix = '(' + infix + ')';
     int l = strLength(infix);
     stack<char> char_stack;
     string output;
- 
-    for (int i = 0; i < l; i++) {
- 
+
+    for (int i = 0; i < l; i++)
+    {
+
         // Checking for operand and adding to stack
         if (isalpha(infix[i]) || isdigit(infix[i]))
             output += infix[i];
- 
-        //Pushing all detected '(' to the stack)
+
+        // Pushing all detected '(' to the stack)
         else if (infix[i] == '(')
             char_stack.push('(');
- 
-        //Starting popping when ')' is detected until next '('
-        else if (infix[i] == ')') {
-            while (char_stack.top() != '(') {
+
+        // Starting popping when ')' is detected until next '('
+        else if (infix[i] == ')')
+        {
+            while (char_stack.top() != '(')
+            {
                 output += char_stack.top();
                 char_stack.pop();
             }
- 
+
             // Remove '(' from the stack
             char_stack.pop();
         }
- 
+
         // Operator found
-        else{
-            if (isOperator(char_stack.top())){
-                if(infix[i] == '^'){
-                      while (getPriority(infix[i]) <= getPriority(char_stack.top())){
-                         output += char_stack.top();
-                         char_stack.pop();
-                       }
-                     
+        else
+        {
+            if (isOperator(char_stack.top()))
+            {
+                if (infix[i] == '^')
+                {
+                    while (getPriority(infix[i]) <= getPriority(char_stack.top()))
+                    {
+                        output += char_stack.top();
+                        char_stack.pop();
+                    }
                 }
-                else{
+                else
+                {
                     while (getPriority(infix[i]) < getPriority(char_stack.top()))
-                       {
-                         output += char_stack.top();
-                         char_stack.pop();
-                       }
-                     
+                    {
+                        output += char_stack.top();
+                        char_stack.pop();
+                    }
                 }
- 
+
                 // Push current Operator on stack
                 char_stack.push(infix[i]);
             }
         }
     }
-      while(!char_stack.empty()){
-          output += char_stack.top();
+    while (!char_stack.empty())
+    {
+        output += char_stack.top();
         char_stack.pop();
     }
     return output;
 }
- 
+
 string infixToPrefix(string infix)
 {
     /* Reverse String and replace opening and closing parentheses */
     int l = infix.size();
- 
+
     // Reverse infix
-    for (int i = 0; i < l / 2; i++) {
+    for (int i = 0; i < l / 2; i++)
+    {
         char temp = infix[i];
         infix[i] = infix[l - i - 1];
         infix[l - i - 1] = temp;
     }
- 
+
     // Replace ( with ) and vice versa
-    for (int i = 0; i < l; i++) {
- 
-        if (infix[i] == '(') {
+    for (int i = 0; i < l; i++)
+    {
+
+        if (infix[i] == '(')
+        {
             infix[i] = ')';
             i++;
         }
-        else if (infix[i] == ')') {
+        else if (infix[i] == ')')
+        {
             infix[i] = '(';
             i++;
         }
     }
- 
+
     string prefix = infixToPostfix(infix);
- 
+
     // Reverse postfix
-    for (int i = 0; i < l / 2; i++) {
+    for (int i = 0; i < l / 2; i++)
+    {
         char temp = prefix[i];
         prefix[i] = prefix[l - i - 1];
         prefix[l - i - 1] = temp;
     }
- 
+
     return prefix;
 }
 
 using namespace std;
-int main(){
+int main()
+{
     string inputExp;
-    cout<<"Please enter expression to be evaluated: ";
-    cin>>inputExp;
+    cout << "Please enter expression to be evaluated: ";
+    cin >> inputExp;
 
     string postfixExp = infixToPostfix(inputExp);
-    cout<<"Postfix expression: "<<postfixExp<<endl;
+    cout << "Postfix expression: " << postfixExp << endl;
 
     string prefixExp = infixToPrefix(inputExp);
-    cout<<"Prefix expression: "<<prefixExp<<endl;
+    cout << "Prefix expression: " << prefixExp << endl;
 
-    
-
-    //searching for AND and OR operators
-    for(int i=0; i<inputExp.length(); i++){
-        if(inputExp[i]=='+'){
-            cout<<"AND operator found at position "<<i<<endl;
-            //and gate truth table for two variables
-            
-
-            }
-        else if(inputExp[i]=='|'){
-            cout<<"OR operator found at position "<<i<<endl;
-            //or gate logic diagram printing
-
+    // searching for AND and OR operators
+    for (int i = 0; i < inputExp.length(); i++)
+    {
+        if (inputExp[i] == '+')
+        {
+            cout << "AND operator found at position " << i << endl;
+            // and gate truth table for two variables
         }
-        else if(inputExp[i]=='^'){
-            cout<<"XOR operator found at position "<<i<<endl;
-            //xor gate logic diagram printing
+        else if (inputExp[i] == '|')
+        {
+            cout << "OR operator found at position " << i << endl;
+            // or gate logic diagram printing
+        }
+        else if (inputExp[i] == '^')
+        {
+            cout << "XOR operator found at position " << i << endl;
+            // xor gate logic diagram printing
         }
     }
 }
